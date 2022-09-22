53303b8ba20523b5a5500688a72d2623b89b838c
BEGGAR AREZKI <il11246@pc523a1-16.ens.math-info.univ-paris5.fr>
2022-09-22 14:47:57 +0200

    projet initial

--- PROJET_BDDA_ZHENG_BARUA_BEGGAR/CODE/src/Main.java
+++ PROJET_BDDA_ZHENG_BARUA_BEGGAR/CODE/src/Main.java
@@ -4,7 +4,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		DBParams.DBPath="/users/licence/il11246/BDDA/PROJET_BDDA_ZHENG_BARUA_BEGGAR/DB";
+		DBParams.pageSize=4096;
+		DBParams.maxPagesPerFile=4;
+		
+		char [] buffer;
 	}
+	
+	
+	
 }
 

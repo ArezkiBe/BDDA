b9c25db0d906fcb288503364205280ca7cb6ffea
Arezki <beggararezki@gmail.com>
2022-10-13 19:54:35 +0200

    Update DiskManager.java

--- PROJET_BDDA_ZHENG_BARUA_BEGGAR_NODJIMENDA/CODE/src/DiskManager.java
+++ PROJET_BDDA_ZHENG_BARUA_BEGGAR_NODJIMENDA/CODE/src/DiskManager.java
@@ -4,40 +4,98 @@ import java.util.StringTokenizer;
 public class DiskManager {
 
   public static DiskManager LeDisqueManager = new DiskManager();
-  private static byte[] buffer ;
+  //private static byte[] buffer ;
   //nous utileserons une table pour les case libre et une table pour les cases remplie
-  public ArrayList<PageId> PageDispo = new ArrayList<PageId>();
-  public ArrayList<PageId> PageNonDispo = new ArrayList<PageId>();
+  public ArrayList<PageId> pageDispo = new ArrayList<PageId>();
+  public ArrayList<PageId> pageNonDispo = new ArrayList<PageId>();
+
 
   public DiskManager getDisque(){
     return LeDisqueManager;
   }
+
+
+
+
   public PageId allocPage(){
-    PageId Page = new PageId();
-    int numero_fichier=0;
-    File fichier = new File(DBParams.DBPath + "/f" +numero_fichier + ".bdda");
-  }
-  public void ReadPage (PageId pageId, buffer){
-    
+    PageId page = new PageId();
+    int numero_fichier = 0;
+    int numero_page = 0;
+
+    if (pageDispo.size()==0){
+      File fichier = new File(DBParams.DBPath + "/f" +numero_fichier + ".bdda");
+
+      if (fichier.exists()){
+        numero_fichier +=1;
+      }
+
+      else{
+        fichier.createNewFile();
+        for (int i=0; i<DBParams.maxPagesPerFile;i++){
+          PageId pages= new PageId(numero_fichier,i);
+          pageDispo.add(pages);
+        }
+        pageNonDispo.add(pageDispo(0));
+        pageDispo.remove(0);
+        page =pageNonDispo.size()
+
+        return page;
+      }
+    }
+
+    else {
+      page = PageDispo.size();
+      PageDispo.remove(0);
+      return page;
+
+    }
   }
 
-  public void WritePage (PageId pageId, buffer) {
+
+
+  public void ReadPage (PageId pageId, ByteBuffer buffer){
+
+    RandomAccessFile lecture = new RandomAccessFile (DBParams.DBPath+"/f"+ pageId.getId+".bdda","r");
+
+    lecture.seek(pageId.getIndice*DBParams.pageSize);
+    for (int i=0; i<DBParams.pageSize;i++){
+      buffer.put(i,lecture.readByte());
+    }
+
+    lecture.close;    
+  }
+
+
+
+
+  public void WritePage (PageId pageId, ByteBuffer buffer) {
+
+    RandomAccessFile lecture = new RandomAccessFile (DBParams.DBPath+"/f"+ pageId.getId+".bdda","rw");
+
+		lecture.seek(pageId.getIndice*DBParams.pageSize);
+		lecture.write(buffer);
+		lecture.close();
     
   }
 
   public void DeallocPage(PageId pageId) {
 
+    int indice;
+    for (int i=0; i<pageNonDispo.size();i++){
+      if (pageNonDispo(i).getId==pageId.getId && pageNonDispo(i).getIndice==pageId.getIndice){
+        indice =i;
+      }
+    }
+    pageNonDispo.remove(indice);
+    pageDispo.add(pageId);
+
   }
 
   public int GetCurrentCountAllocPages() {
 
+    return pageNonDispo.size();
+
   }
 
-  /*
-   * public PageId AllocPage(){
-   * 
-   * 
-   * }
-   */
 
 }
